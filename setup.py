a87c4b7a4b977c740527b3b4e9a35e16760ff9bc
Kapil Thangavelu <kapilt@gmail.com>
2022-09-01 06:22:15 -0400

    increment version to match extant pypi dummy registration, hoist common env var

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Wayne Witzel III",
     author_email="wayne@stacklet.io",
     license="Apache-2.0",
-    version="0.0.1",
+    version="0.1.1",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.10",
