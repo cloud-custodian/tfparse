c389feb3fd0e0420b649bfc15e6407cdaf8bacca
Wayne Witzel III <wayne@stacklet.io>
2022-10-12 06:08:56 -0700

    make notify_slack test fail

--- tests/test_tfparse.py
+++ tests/test_tfparse.py
@@ -117,4 +117,5 @@ def test_parse_apprunner(tmp_path):
 def test_parse_notify_slack(tmp_path):
     mod_path = init_module("notify_slack", tmp_path)
     parsed = load_from_path(mod_path)
-    assert parsed is not None
+    # TODO: determine the expected output of the JSON from the notify_slack terraform test
+    assert parsed == {}
