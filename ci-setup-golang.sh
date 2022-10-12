6beff99c80bba30996f83435cb317cbfd5fd035d
Wayne Witzel III <wayne@stacklet.io>
2022-10-12 08:55:06 -0700

    update .gitignore

--- ci-setup-golang.sh
+++ ci-setup-golang.sh
@@ -2,7 +2,7 @@
 
 OS=$(uname -s)
 ARCH=$(uname -m)
-GOVER="1.18.5"
+GOVER="1.19.2"
 case $ARCH in
     x86_64) ARCH="amd64" ;;
     aarch64) ARCH="arm64" ;;
@@ -12,7 +12,7 @@ case $OS in
     Linux) OS="linux" ;;
 esac    
 	    
-curl "https://storage.googleapis.com/golang/go1.18.5.${OS}-${ARCH}.tar.gz" --silent --location | tar -xz
+curl "https://storage.googleapis.com/golang/go1.19.2.${OS}-${ARCH}.tar.gz" --silent --location | tar -xz
 
 export PATH="$(pwd)/go/bin:$PATH"
 
