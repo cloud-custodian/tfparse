1b2725500546cbef41284c82bb36f58810c93355
Joe Lombrozo <joe@djeebus.net>
2022-10-13 19:17:39 -0400

    add some mega tests smaller tests will be coming

--- tests/terraform/notify_slack/main.tf
+++ tests/terraform/notify_slack/main.tf
@@ -1,3 +1,7 @@
+locals {
+  sns_kms_key_arn = "abc-123-arn"
+}
+
 module "notify_slack_saas" {
   source  = "terraform-aws-modules/notify-slack/aws"
   version = "~> 5.3.0"
@@ -6,7 +10,7 @@ module "notify_slack_saas" {
   sns_topic_name       = "slack-alert-saas"
   slack_webhook_url    = "https://localhost"
   slack_channel        = "feed-ops-saas"
-  slack_username       = "${data.aws_caller_identity.current.account_id}-sns"
+  slack_username       = "slack-username"
   lambda_function_name = "notify_slack_saas"
   sns_topic_kms_key_id = local.sns_kms_key_arn
 
@@ -27,7 +31,7 @@ module "notify_slack_qa" {
   sns_topic_name       = "slack-alert-qa"
   slack_webhook_url    = "https://localhost"
   slack_channel        = "feed-ops-qa"
-  slack_username       = "${data.aws_caller_identity.current.account_id}-sns"
+  slack_username       = "slack-username"
   lambda_function_name = "notify_slack_qa"
   sns_topic_kms_key_id = local.sns_kms_key_arn
 
---
+++ tests/test_modules.py
@@ -0,0 +1,124 @@
+import os
+import pytest
+import uuid
+
+from tests.test_tfparse import init_module, load_from_path
+
+here = os.path.dirname(__file__)
+
+
+@pytest.mark.parametrize(('dir_name', 'wipes'), [
+    ('apprunner', []),
+    ('dynamic-stuff', []),
+    ('eks', []),
+    ('notify_slack', [
+        'data.aws_iam_policy.tracing.arn',
+        'data.aws_iam_policy.vpc.arn',
+        'locals.lambda_policy_document.resources',
+        'null_resource.archive[0].provisioner.local-exec.command',
+    ]),
+    ('period-separator', []),
+    ('vpc_module', []),
+])
+def test_verify_terraform(dir_name, wipes, tmp_path):
+    mod_path = init_module(dir_name, tmp_path)
+    actual = load_from_path(mod_path)
+    _clean_volatiles(actual, wipes)
+
+    import json
+    actual_path = os.path.join(here, 'terraform', dir_name, 'actual.json')
+    if os.path.exists(actual_path):
+        os.unlink(actual_path)
+    with open(actual_path, 'w') as fp:
+        json.dump(actual, fp, indent=2)
+
+    expected_path = os.path.join(here, 'terraform', dir_name, 'expected.json')
+    if os.path.exists(expected_path):
+        with open(expected_path, 'r') as fp:
+            expected = json.load(fp)
+    else:
+        with open(expected_path, 'w') as fp:
+            json.dump(actual, fp, indent=2)
+        expected = actual
+
+    assert actual == expected
+
+
+def _clean_volatiles(obj, wipes):
+    ids = {_counter: 1}
+
+    _collect_all_ids(obj, ids)
+    _replace_all_ids(obj, ids)
+
+    for wipe in wipes:
+        _wipe_by_path(obj, wipe)
+
+
+def _wipe_by_path(obj, path):
+    *parts, final = path.split('.')
+    for part in parts:
+        obj = obj[part]
+    obj[final] = None
+
+
+_counter = object()
+
+
+def is_uuid(val):
+    try:
+        uuid.UUID(val)
+        return True
+    except ValueError:
+        return False
+
+
+def next_id(val, ids):
+    # grab the next id
+    next_id = ids[_counter]
+
+    # increment the counter
+    ids[_counter] = next_id + 1
+
+    # map the volatile id to a non-volatile id
+    new_id = str(uuid.UUID(int=next_id))
+    ids[val] = new_id
+    return new_id
+
+
+def _collect_all_ids(obj, ids):
+    if isinstance(obj, dict):
+        for key in sorted(obj):
+            val = obj[key]
+            if key == "id" and is_uuid(val):
+                obj[key] = next_id(val, ids)
+                continue
+
+            _collect_all_ids(val, ids)
+        return
+
+    elif isinstance(obj, list):
+        for index, item in enumerate(obj):
+            if isinstance(item, str) and is_uuid(item):
+                obj[index] = next_id(item, ids)
+                continue
+
+            _collect_all_ids(item, ids)
+        return
+
+
+def _replace_all_ids(obj, ids):
+    if isinstance(obj, dict):
+        for key, val in list(obj.items()):
+            if isinstance(val, str):
+                new_id = ids.get(val)
+                if new_id is not None:
+                    obj[key] = new_id
+                    continue
+
+            _replace_all_ids(val, ids)
+        return
+
+    if isinstance(obj, list):
+        for item in obj:
+            _replace_all_ids(item, ids)
+        return
