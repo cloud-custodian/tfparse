bb8e0df574ef6c259f067f251de855128dcc87ea
Kapil Thangavelu <kapilt@gmail.com>
2023-08-18 11:30:39 -0400

    black fmt

--- tfparse/__init__.py
+++ tfparse/__init__.py
@@ -38,9 +38,13 @@ def load_from_path(
 
     vars_paths = vars_paths or []
     num_var_paths = len(vars_paths)
-    c_var_paths = [ffi.new("char[]", str(vars_path).encode("utf8")) for vars_path in vars_paths]
+    c_var_paths = [
+        ffi.new("char[]", str(vars_path).encode("utf8")) for vars_path in vars_paths
+    ]
 
-    ret = lib.Parse(s, stop_on_hcl_error, debug, allow_downloads, num_var_paths, c_var_paths)
+    ret = lib.Parse(
+        s, stop_on_hcl_error, debug, allow_downloads, num_var_paths, c_var_paths
+    )
 
     if ret.err != ffi.NULL:
         err = ffi.string(ret.err)
