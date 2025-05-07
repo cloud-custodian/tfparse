a9ccb10c1cef931aaa59ad1cb1d06d3d0c9c8cb6
John Anderson <sontek@gmail.com>
2025-05-07 11:45:21 -0400

    feat: provide more information for unresolved values (#247)
    * provide more information for unresolved values
    
    When referencing variables that haven't been defined or data sources
    that aren't available, instead of just putting in `None` lets return
    what value we didn't resolve.
    
    This also leaves strings with unresolved values uninterpolated so we
    know exactly what they were trying to do.
    
    * linting

--- tests/terraform/apply-time-vals/main.tf
+++ tests/terraform/apply-time-vals/main.tf
@@ -46,3 +46,39 @@ resource "aws_db_parameter_group" "untagged" {
   name   = "untagged"
   family = "postgres16"
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "attribute_with_direct_reference" {
+  permissions_boundary = data.aws_caller_identity.current.account_id
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "attribute_with_interpolated_reference" {
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/BoundaryPolicy"
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "attribute_not_present" {}
