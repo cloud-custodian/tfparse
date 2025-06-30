570987ee8e797caf8c74fd7a43af3dce44d53d68
Stephan Windischmann <stephan.windischmann@bentley.com>
2025-06-30 22:49:29 +1200

    Fix formatting

--- tests/test_tfparse.py
+++ tests/test_tfparse.py
@@ -693,7 +693,7 @@ def test_handle_sensitive_value(tmp_path):
 def test_wholly_known_foreach(tmp_path):
     mod_path = init_module("wholly-known-for_each", tmp_path, run_init=False)
     parsed = load_from_path(mod_path)
-    assert parsed["locals"][0]["current_month"] ==6
+    assert parsed["locals"][0]["current_month"] == 6
     assert parsed["locals"][0]["last_month"] == 5
     assert parsed["terraform_data"][0]["for_each"] == [5, 6]
 
