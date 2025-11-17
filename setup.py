66a074c41e60f1e4fc18e357cd916018855577ad
AJ Kerrigan <aj@stacklet.io>
2025-11-17 10:56:53 -0500

    bump version 0.6.18 --> 0.6.19

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     maintainer="Cloud Custodian Project",
     maintainer_email="cloud-custodian@googlegroups.com",
     license="Apache-2.0",
-    version="0.6.18",
+    version="0.6.19",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
